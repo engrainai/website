9b12aac5f4d85d59c8b2fae00e461bb86f09c19a
engrainai <logan@engrainai.com>
2026-01-02 09:48:26 -0700

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -65,6 +65,7 @@ export function Footer() {
                 >
                   Contact Us
                 </button>
+              </li>
               <li>
                <button
                   onClick={() => setLocation("/privacy-policy")}
